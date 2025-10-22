0c18da3c83bcd856a48274ef6a51235afcc914b8
Victor Nascimento <victor.nascimento.ns@gmail.com>
2025-10-22 13:19:33 -0300

    style: update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { Red_Hat_Display } from "next/font/google";
 
 import "./globals.scss";
-
-const redHatDisplaySans = Red_Hat_Display({
-  variable: "--font-red-hat-display",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600"],
-});
 
 export const metadata: Metadata = {
   title: "",
@@ -20,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={`${redHatDisplaySans.variable} antialiased`}>
+    <html lang="pt-BR" className="antialiased">
       <body>{children}</body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-red-hat-display)"]
+        sans: ["Courier New", "Courier", "monospace"],
       },
     },
   },
