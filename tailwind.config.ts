39cc48c501ed75e6157aaf1675652ec2f1c85e2c
Victor Nascimento <victor.nascimento.ns@gmail.com>
2025-10-31 00:47:41 -0300

    style(tailwind): update global font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Courier New", "Courier", "monospace"],
+        sans: ["Verdana", "sans-serif"],
       },
     },
   },
